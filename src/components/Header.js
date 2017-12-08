aa2cca62d2a5e24e29fe1bde8b344f78ad37e187
Julia Wolf Martinez <juliahmartinez@yahoo.com>
2017-12-08 15:52:27 -0700

    Fixed it.
    Imanged to get the Header and Footer to show up on the page. Finally. I am Happy.

---
+++ src/components/Header.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+const MyHeader = () => (
+  <Header
+    // This is the sticky part of the header.
+    header={
+      <div className="Header_root">
+        <h1 className="Header_title">RMuse Live</h1>
+      </div>
+    }
+  />
+);
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container" />
+        <nav className="nav navbar-light">
+          <ul className="Header_links">
+            <li className="Header_link">Home</li>
+            <li className="Header_link">Login</li>
+            <li className="Header_link">Sign Up</li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+export default Header;
