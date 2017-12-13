06345338d524528e0925f330d0d2c1d63310fb50
John Lyons <jackly220@gmail.com>
2017-12-13 11:54:24 -0700

    here's the new code

---
+++ src/Component.js/LoginButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+function LogginButton(props) {
+  return <button onClick={e => this.handleClick(e)}>Click Me</button>;
+  function UserGreeting(props) {
+    return <h3>Login</h3>;
+  }
+  function GuestGreeting(props) {
+    return <h3>Please Sign up</h3>;
+  }
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+export default LogginButton;
