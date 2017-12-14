02da4d2cecee8a85e044def6122fd3c9e9b40b82
Siobhan Lyons <siobhanlyons@Siobhans-MacBook-Air.local>
2017-12-14 11:51:12 -0700

    modified conflicts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <navBar />
+        {this.props.children}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
