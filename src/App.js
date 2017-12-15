8a25fa18c715f1fc921f05781d62668655899250
Robert Hawk <robert@the-hawk.us>
2017-12-15 12:32:17 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
-<<<<<<< HEAD
 // import User from "./components/User";
 //import Navbar from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Support from "./components/Support";
-=======
 import User from "./components/User";
 import FAQ from "./components/FAQ";
->>>>>>> 91f14ffb8e5340e60df94ee33efafdaf5def8aeb
 
 class App extends Component {
   render() {
@@ -25,15 +22,12 @@ class App extends Component {
           <div>
             <Switch>
               <Route exact path="/" component={Home} />
-<<<<<<< HEAD
               {/* <Route path="/User" component={User} /> */}
               <Route path="/about" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/support" component={Support} />
-=======
               <Route path="/User" component={User}/>
               <Route path="/faq" component={FAQ}/>
->>>>>>> 91f14ffb8e5340e60df94ee33efafdaf5def8aeb
             </Switch>
           </div>
         </Router>
